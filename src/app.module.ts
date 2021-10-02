df51cc90061e6aee7e529db74190ea3241a428a4
Lucas C Correa <lucascorrea@uzotto.com>
2021-10-02 15:27:46 -0300

    added postgress with typeorm initial configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
+import { dbConfig } from './config';
+
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, TypeOrmModule.forRoot(dbConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
